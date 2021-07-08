6906e39df8d3516ffefbbfff272cfcdb14932c92
Vladimir <kozinskytxc@gmail.com>
2021-07-08 14:56:18 +0400

    refactoring of profilePage

--- my-app/src/components/Profile/Profileinfo/ProfileInfo.jsx
+++ my-app/src/components/Profile/Profileinfo/ProfileInfo.jsx
@@ -1,6 +1,11 @@
 import s from './ProfileInfo.module.css';
+import webLogo from './../../common/profile/contactsLogos/webLogo.jpg';
+import facebookLogo from './../../common/profile/contactsLogos/facebookLogo.png';
+import instaLogo from './../../common/profile/contactsLogos/instLogo.svg';
+import vkLogo from './../../common/profile/contactsLogos/vkLogo.png';
 
 const ProfileInfo = (props) => {
+
     return (
         <div className={s.profileInfo}>
             <div className={s.profileImage}>
@@ -9,24 +14,45 @@ const ProfileInfo = (props) => {
 
             <div className={s.personalInfo}>
                 <div className={s.userName}>
-                    name  
+                    {props.profileInfo.fullName}
                 </div>
                 <div className={s.userStatus}>
-                    status - {props.profileInfo.aboutMe}
+                    {props.profileInfo.aboutMe
+                        ? props.profileInfo.aboutMe
+                        : ' '}
                 </div>
                 <div className={s.lookingForAJob}>
-                true
+                    <div className={s.lookingForAJobDiv}>LookingForAJob:</div>{(props.profileInfo.lookingForAJob) ? <div className={s.lookingForAJobText}>Yes</div> : <div className={s.lookingForAJobText}>No</div>}
                 </div>
                 <div className={s.lookingForAJobDescription}>
-                не ищу, а дурачусь
+                    <div className={s.lookingForAJobDescriptionDiv}>Job Description:</div>{props.profileInfo.lookingForAJobDescription
+                        ? props.profileInfo.lookingForAJobDescription
+                        : "No filled"}
                 </div>
                 <div className={s.contacts}>
-                    contacts
-                </div>
-                
-              
-            </div>
+                    <div className={s.contactsText}>Contacts:</div>
+                    <div className={s.contactLogoFacebook}><img src={facebookLogo} alt="" /></div>
+                    <div className={s.contactLogoVk}><img src={vkLogo} alt="" /></div>
+                    <div className={s.contactLogoWeb}><img src={webLogo} alt="" /></div>
+                    <div className={s.contactLogoInsta}><img src={instaLogo} alt="" /></div>
+                    <div className={s.linkFacebook}>{props.profileInfo.contacts.facebook
+                        ? props.profileInfo.contacts.facebook
+                        : "No"}</div>
 
+                    <div className={s.linkVk}>{props.profileInfo.contacts.vk
+                        ? props.profileInfo.contacts.vk
+                        : "No"}</div>
+
+                    <div className={s.linkInsta}>{props.profileInfo.contacts.instagram
+                        ? props.profileInfo.contacts.instagram
+                        : "No"}</div>
+
+                    <div className={s.linkWeb}>{props.profileInfo.contacts.website
+                        ? props.profileInfo.contacts.website
+                        : "No"}</div>
+
+                </div>
+            </div>
         </div>
     )
 }
