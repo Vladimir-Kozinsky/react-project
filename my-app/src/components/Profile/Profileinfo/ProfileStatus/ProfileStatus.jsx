f27924b6aa5f1541e4aa881220ab74cc88c98acf
Vladimir Kozinsky <kozinskytxc@gmail.com>
2021-07-10 16:33:35 +0400

    added componentDidUpdate

--- my-app/src/components/Profile/Profileinfo/ProfileStatus/ProfileStatus.jsx
+++ my-app/src/components/Profile/Profileinfo/ProfileStatus/ProfileStatus.jsx
@@ -26,6 +26,14 @@ class ProfileStatus extends React.Component {
         })
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.state.status,
+            });
+        }
+    }
+
     render() {
         return (
             <div className={s.userStatus}>
